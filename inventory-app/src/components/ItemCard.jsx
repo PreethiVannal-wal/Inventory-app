3c110a565e9c18c6e394a6b1fd53cc8f95ca8a7e
PreethiVannal <preethivr151302@gmail.com>
2021-12-10 11:07:39 +0530

    styling

--- inventory-app/src/components/ItemCard.jsx
+++ inventory-app/src/components/ItemCard.jsx
@@ -1,6 +1,7 @@
 import {Col, CardGroup, Card, CardImg, CardBody, CardTitle, CardText,Button} from 'reactstrap';
 import {useState} from 'react';
 import UpdateForm from './UpdateForm';
+import './style.css';
 const ItemCard = ({id, name, quantity, imageUrl,onDeleting, updateItem}) => {
 
     const [updateFormOpen, setUpdateFormOpen] = useState(false);
@@ -17,26 +18,25 @@ const ItemCard = ({id, name, quantity, imageUrl,onDeleting, updateItem}) => {
         setUpdateFormOpen(false);
     }
     return(
-        <Col className="bg-light border">
-            <CardGroup>
-                <Card>
-                    <CardImg
-                        alt={name}
-                        src={imageUrl}
-                        width="100%"
-                    />
-                    <CardBody>
-                        <CardTitle tag="h4">
-                            {name}
-                        </CardTitle>
-                        <CardText>
-                            Stock Available: {quantity}
-                        </CardText>
-                        <Button onClick = {EditingItem}>Update</Button>
-                        <Button onClick = {deleteItem}>Delete</Button>
-                    </CardBody>
-                </Card>
-            </CardGroup>
+        <Col>
+            <Card className="item-card">
+                <CardImg
+                    className="item-image"
+                    alt={name}
+                    src={imageUrl}
+                    width="100%"
+                />
+                <CardBody>
+                    <CardTitle tag="h4">
+                        {name}
+                    </CardTitle>
+                    <CardText>
+                        Stock Available: {quantity}
+                    </CardText>
+                    <Button onClick = {EditingItem}>Update</Button>
+                    <Button onClick = {deleteItem}>Delete</Button>
+                </CardBody>
+            </Card>
             {updateFormOpen && (
                 <UpdateForm name ={name} quantity = {quantity} onUpdate={updatingItem}/>
             )}
