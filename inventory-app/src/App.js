174793b9388a6e2d966cc3d71513ccfb64bb059b
PreethiVannal <preethivr151302@gmail.com>
2021-12-10 10:19:26 +0530

    styling

--- inventory-app/src/App.js
+++ inventory-app/src/App.js
@@ -1,9 +1,10 @@
 import ItemsList from "./components/ItemsList";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Inventory App</h1>
+      <h1 class="main-heading">Inventory App</h1>
       <ItemsList/>
     </div>
   );
